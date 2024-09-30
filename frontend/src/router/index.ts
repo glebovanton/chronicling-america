b334ec9caf5780ae7e9aed232126edf900f1adf0
anton-glebov <anton.glebov@useinsider.com>
2024-09-30 21:28:04 +0300

    refactor fe

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import SearchView from '../views/SearchView.vue';
 import CompanyDetailView from '../views/CompanyDetailView.vue';
 
@@ -17,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes,
 });
 
