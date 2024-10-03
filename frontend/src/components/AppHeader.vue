4a80aaf35e6e372923916b2566122cce709b9b78
anton-glebov <anton.glebov@useinsider.com>
2024-10-03 21:42:19 +0300

    rename header

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { RouterLink, useRoute } from "vue-router";
 
 const linksData = [
@@ -18,7 +18,7 @@ function isActive(name: string): boolean {
 <template>
   <header class="z-[1] relative">
     <div
-      class="flex fixed top-0 left-1/2 h-[4.5rem] px-3 w-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] top-6 h-[3.25rem] w-4/5 rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+      class="flex fixed left-1/2 px-3 border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] top-6 h-[3.25rem] w-4/5 rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
       style="
         opacity: 1;
         transform: translateX(-50%) translateY(0px) translateZ(0px);
@@ -31,13 +31,13 @@ function isActive(name: string): boolean {
           class="flex flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray900 sm:w-[initial] sm:flex-nowrap sm:gap-5"
         >
           <li
-            v-for="({ name, hash, title }, index) in linksData"
+            v-for="({ name, title }, index) in linksData"
             :key="`link-${index}`"
             class="h-3/4 flex items-center justify-center relative"
             style="opacity: 1; transform: none"
           >
             <RouterLink
-              class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300 text-gray-950 dark:text-gray-200"
+              class="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-300 text-gray-950 dark:text-gray-200"
               :to="{ name }"
             >{{ title }}
               <span
