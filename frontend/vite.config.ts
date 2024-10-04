44ef1f09f79b0febe16c7a9f4ef879adf4748af3
anton-glebov <anton.glebov@useinsider.com>
2024-10-04 18:27:20 +0300

    fix deploy flow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'git' ? '/chronicling-america-fe/' : '/',
   plugins: [
     vue(),
     vueJsx(),
